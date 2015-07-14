c113291cff5644392dc896788cf82a84196acde3
lorenzo <lupacche@gmail.com>
2015-07-14 16:43:19 +0200

    autoCommit

--- run.sh
+++ run.sh
@@ -3,7 +3,5 @@ echo "making..."
 make
 echo -n "PARAMETRI: < tipo_ds input_points dimensioni centroidi generazioni > "
 read text
-#echo "#DEFC v9"
-#time ./defc9.x $text &
 echo "#DEFC v9b"
-time ./defc9b.x $text
+time ./defc9b.x $text &
